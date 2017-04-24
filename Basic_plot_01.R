30f059b9e9c8df6a7fe269bc8a034b71f65c6a0b
Seung Wook Kim <CEO@Rloha.io>
2017-04-24 18:04:38 +0900

    Delete Basic_plot_01.R

--- Basic_plot_01.R
+++
@@ -1,6 +0,0 @@
-plot_a = data.frame( xx = 1:10, yy = 31:40 )
-plot_b = data.frame( xx = 1:10, yy = 15:24 )
-  
-plot(plot_a, ylim = c(0, 50), col = "red" , type = "b")
-par(new = TRUE)
-plot(plot_b, ylim = c(0, 50), col = "blue", type = "b")
