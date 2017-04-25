0445b33c181b0878a2697f06098a5272d501f2ea
Seung Wook Kim <CEO@Rloha.io>
2017-04-25 13:23:07 +0900

    Update text_handling_html_text_extractor.R

--- text_handling_html_text_extractor.R
+++ text_handling_html_text_extractor.R
@@ -5,7 +5,7 @@ text_extractor = function(x, start_pattern = ">", end_pattern = "<", adj_start =
   if(start_pattern == ">"){
     pattern_count = length(pattern_position_start)
     text = substr(x = x, 
-                  start = pattern_position_start[pattern_count/2] + adj_start,
+                  start = pattern_position_start[pattern_count/2]   + adj_start,
                   stop  = pattern_position_end[pattern_count/2 + 1] + adj_end)
   } else { 
     text = substr(x = x, 
