f3534a47d56e08cdedb158f3958e8933a476bfbe
Seung Wook Kim <CEO@Rloha.io>
2017-12-07 12:49:48 +0900

    Update RSelenium_example_01.R

--- 01_R/crawling/RSelenium_example_01.R
+++ 01_R/crawling/RSelenium_example_01.R
@@ -2,12 +2,11 @@
 # using phantom browser
 port_no = sample(2000:8000, size = 1)
 
-library("wdman")
-selenium(port = port_no)
-
 library("RSelenium")
 dd = rsDriver(port = port_no, browser = "phantomjs")
 dd_client = dd$client
+
+dd_client$setImplicitWaitTimeout(milliseconds = 3000) # 모든 자원 로드를 위해 설정하는 옵션
 dd_client$navigate("http://www.google.com")
 dd_client$navigate("https://www.naver.com")
 dd_client$goBack()
