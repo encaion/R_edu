10cbffb692dc03b10bc38e79b4570f38a37f45be
Seung Wook Kim <CEO@Rloha.io>
2018-01-12 21:31:44 +0900

    Update Naver_top20_issue_170503.R

--- 01_R/crawling/Naver_top20_issue_170503.R
+++ 01_R/crawling/Naver_top20_issue_170503.R
@@ -1,1 +1,1 @@
-gsub(pattern = "<.*?>", replacement = "", grep(pattern = "ah_k", readLines("https://www.naver.com", encoding = "UTF-8"), value = TRUE)[1:20])
+gsub("<.*?>", "", grep("ah_k", readLines("https://www.naver.com", encoding = "UTF-8"), value = TRUE)[1:20])
