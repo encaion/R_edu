271ca779cf39f8a6a3f5f19dc0952a669e0965bd
Seung Wook Kim <CEO@Rloha.io>
2018-01-12 21:30:59 +0900

    Update Naver_top20_issue_170503.R

--- 01_R/crawling/Naver_top20_issue_170503.R
+++ 01_R/crawling/Naver_top20_issue_170503.R
@@ -1,3 +1,1 @@
-text = readLines("https://www.naver.com")
-Encoding(text) = "UTF-8"
-gsub(pattern = "<.*?>", replacement = "", grep(pattern = "ah_k", text, value = TRUE)[1:20])
+gsub(pattern = "<.*?>", replacement = "", grep(pattern = "ah_k", readLines("https://www.naver.com", encoding = "UTF-8"), value = TRUE)[1:20])
