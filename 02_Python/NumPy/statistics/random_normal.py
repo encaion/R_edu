a76c6b17935202f83d65abb83c5a852a251494a4
Seung Wook Kim <CEO@Rloha.io>
2018-01-15 23:06:21 +0900

    Create random_normal.py

---
+++ 02_Python/NumPy/statistics/random_normal.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+np.random.normal(loc = 0.0, # mean
+                 scale = 1, # standard deviation
+                 size  = (2, 3))
+# array([[-0.94819353,  0.96726056, -0.08305624],
+#        [ 0.76933826, -0.75758055, -1.15402694]])
