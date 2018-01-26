71407e644cb9d5da47569bc4dfc60c7f8f788c3e
Seung Wook Kim <CEO@Rloha.io>
2018-01-26 18:57:19 +0900

    Update for_loop.py

--- 02_Python/grammer/for_loop.py
+++ 02_Python/grammer/for_loop.py
@@ -2,7 +2,10 @@ list_sample_1 = [1, 2, 3]
 for num in list_sample_01:
   print(num)
 
-  
 list_sample_2 = range(0, 5)
 for num in list_sample_02:
   print(num)
+
+list_sample_3 = range(0, 5)
+for num in list_sample_03:
+  print("current loop: %d \r" %num, end = "")
