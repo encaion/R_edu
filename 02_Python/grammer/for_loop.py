87545e22a9b1d85b902ac2f78b1f40be8c365a89
Seung Wook Kim <CEO@Rloha.io>
2018-01-26 18:25:14 +0900

    Update for_loop.py

--- 02_Python/grammer/for_loop.py
+++ 02_Python/grammer/for_loop.py
@@ -1,3 +1,8 @@
-list_sample = [1, 2, 3]
-for num in list_sample:
+list_sample_1 = [1, 2, 3]
+for num in list_sample_01:
+  print(num)
+
+  
+list_sample_2 = range(0, 5)
+for num in list_sample_02:
   print(num)
