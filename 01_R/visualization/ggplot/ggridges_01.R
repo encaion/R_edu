086491700522f05509f4cd9577f153deb7292a54
Seung Wook Kim <CEO@Rloha.io>
2018-02-15 18:59:13 +0900

    Create ggridges_01.R

---
+++ 01_R/visualization/ggplot/ggridges_01.R
@@ -0,0 +1,25 @@
+# install.packages("lubridate")
+# install.packages("viridis")
+# install.packages("ggridges")
+library("lubridate")
+library("viridis")
+library("ggridges")
+
+df_weather = read.csv("https://drive.google.com/file/d/0ByOfjCmqEilLYndpOWJyZXhPVUk/view", stringsAsFactors = FALSE)
+df_weather[, "Month"] = month(df_weather$CST)
+df_weather[, "Month_order"] = factor(month.name[df_weather$Month],
+                                     levels = month.name)
+
+ggplot(df_weather, 
+       aes(x = Mean.TemperatureF, 
+           y = Month_order, 
+           fill = ..x..)) +
+  geom_density_ridges_gradient(scale = 3, 
+                               rel_min_height = 0.01, 
+                               gradient_lwd = 1) +
+  scale_x_continuous(expand = c(0.01, 0.01)) +
+  scale_y_discrete(  expand = c(0.01, 0.01)) +
+  scale_fill_viridis(name = "Temp. [F]", option = "C") +
+  labs(title = 'Temperatures in Lincoln NE') +
+  theme_ridges(font_size = 13, grid = TRUE) + 
+  theme(axis.title.y = element_blank())
