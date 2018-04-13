f1d2d56b3327763cc54f44684ea46306f05868b4
Seung Wook Kim <CEO@Rloha.io>
2018-04-13 13:39:17 +0900

    Update memory.R

--- 01_R/grammers/etc/memory.R
+++ 01_R/grammers/etc/memory.R
@@ -1,6 +1,8 @@
 memory.size()
 # memory.limit()
 
+
+# source: https://stackoverflow.com/questions/6457290/how-to-check-the-amount-of-ram-in-r
 # R> memfree <- as.numeric(system("awk '/MemFree/ {print $2}' /proc/meminfo", intern=TRUE))
 # R> memfree
 # [1] 3342480
