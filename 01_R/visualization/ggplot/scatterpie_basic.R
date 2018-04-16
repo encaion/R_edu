cc12e17b606f50b9f11a8473c40399592425fa58
Seung Wook Kim <CEO@Rloha.io>
2018-04-16 13:26:46 +0900

    Update scatterpie_basic.R

--- 01_R/visualization/ggplot/scatterpie_basic.R
+++ 01_R/visualization/ggplot/scatterpie_basic.R
@@ -3,6 +3,21 @@
 library("ggplot2")
 library("scatterpie")
 
+set.seed(123)
+long = rnorm(50, sd=100)
+lat = rnorm(50, sd=50)
+d = data.frame(long = long, lat = lat)
+d = with(d, d[abs(long) < 150 & abs(lat) < 70,])
+n = nrow(d)
+d$region = factor(1:n)
+d$A = abs(rnorm(n, sd = 1))
+d$B = abs(rnorm(n, sd = 2))
+d$C = abs(rnorm(n, sd = 3))
+d$D = abs(rnorm(n, sd = 4))
+d$radius = 6 * abs(rnorm(n))
+head(d)
+
+
 world = map_data("world")
 p = ggplot(world, aes(long, lat)) +
     geom_map(map = world, aes(map_id = region), fill = NA, color = "black") +
