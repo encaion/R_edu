b7b9d1a658fcb28a3416da5aaca6fda77d10701f
Seung Wook Kim <CEO@Rloha.io>
2018-04-18 21:03:21 +0900

    Update mimic_apple_watch_01.R

--- 01_R/visualization/ggplot/mimic_apple_watch_01.R
+++ 01_R/visualization/ggplot/mimic_apple_watch_01.R
@@ -1,12 +1,14 @@
 library("ggplot2")
 ggplot() + 
-  geom_point(aes(x = 1:8,
-                 y = 3),
+  geom_point(aes(x = 2:9,
+                 y = 3,
+                 color = 1:8),
              size = 50,
-             alpha = 0.5,
-             color = "#B3F5FA") + 
+             alpha = 0.5) + 
   scale_x_continuous(limits = c(1, 9)) + 
   scale_y_continuous(limits = c(2, 4)) + 
+  scale_color_gradient(low = "#A7DDFF", high = "#7FF1DA") +
   coord_polar() +
   theme_void() +
-  theme(panel.background = element_rect(fill = "#000000"))
+  theme(panel.background = element_rect(fill = "#000000"),
+        legend.position = "none")
