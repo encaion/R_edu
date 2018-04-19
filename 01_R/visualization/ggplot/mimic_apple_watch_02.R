16fa3dd6a4cdef5b81d77d95a05ad8664af9f163
Seung Wook Kim <CEO@Rloha.io>
2018-04-19 14:54:38 +0900

    Update mimic_apple_watch_02.R

--- 01_R/visualization/ggplot/mimic_apple_watch_02.R
+++ 01_R/visualization/ggplot/mimic_apple_watch_02.R
@@ -5,18 +5,27 @@ df = data.frame(xx = 1:3,
 ggplot(data = df,
        aes(x = xx,
            y = yy)) + 
-  geom_hline(yintercept = seq(0, max(df$yy) + 1, length.out = 9),
+  geom_hline(yintercept = seq(0, max(df$yy) + 1, length.out = 13),
              color = "#EEEEEE") +
-  geom_col(aes(fill = yy),
-           width = 0.99) + 
-  geom_point(aes(color = yy),
-             size = 15) +
-  scale_x_continuous(limits = c(min(df$xx) - 1, max(df$xx) + 0.5)) +
+  geom_col(aes(y = max(df$yy) + 1),
+           fill = "#000000",
+           width = 1) + 
+  geom_col(aes(y = max(df$yy) + 1,
+               fill = yy,
+               alpha = 0.2),
+           width = 0.95) +
+  geom_col(aes(fill = yy), width = 0.95) + 
+  geom_point(aes(color = yy), size = 15) +
+  geom_point(aes(y = 0, color = yy), size = 15) +
+  scale_x_continuous(limits = c(min(df$xx) - 1, max(df$xx) + 1)) +
   scale_y_continuous(limits = c(0, max(df$yy) + 1)) +
-  geom_vline(xintercept = c(0.5:2.5), size = 1, color = "#EEEEEE") +
-  geom_hline(yintercept = 0) +
-  geom_vline(xintercept = 3.5, size = 3, color = "#CCCCCC") +
+  geom_vline(xintercept = 3.9, size = 4, color = "#EEEEEE",
+             alpha = 0.4) +
+  geom_vline(xintercept = 3.65, size = 5, color = "#000000") +
   coord_flip() +
   coord_polar(theta = "y") +
   theme_void() + 
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        panel.background = element_rect(fill = "#000000"))
+
+ggsave("apple_watch_02.png", dpi = 600)
