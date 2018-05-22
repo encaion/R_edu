b8f4054c59f55a334b0d8c340ef9aebf107d88ab
Seung Wook Kim <CEO@Rloha.io>
2018-05-22 14:55:19 +0900

    Create db_connection_mysql.R

---
+++ 01_R/99_ect/db_connection_mysql.R
@@ -0,0 +1,15 @@
+# install.packages("DBI")
+# install.packages("RMySQL")
+
+library("DBI")
+library("RMySQL")
+dri = dbDriver("MySQL")
+con = dbConnect(drv = dri,
+                host = "localhost",
+                port = 3306,
+                dbname = "test",
+                user = "second",
+                password = "asdf1234")
+df = dbGetQuery(conn = con,
+           "select * from user")
+sapply(df, "class")
