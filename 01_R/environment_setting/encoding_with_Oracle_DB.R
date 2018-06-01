111c71439d84ffd3647e21c36af2be89344cab09
Seung Wook Kim <CEO@Rloha.io>
2018-06-01 20:49:24 +0900

    Create encoding_with_Oracle_DB.R

---
+++ 01_R/environment_setting/encoding_with_Oracle_DB.R
@@ -0,0 +1,7 @@
+# If you encounter broken Korean letters, use this code below.
+
+Sys.setenv(PATH="/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/tagging/bin:/usr/lib/oracle/12.1/client64/bin:/home/tagging/bin")
+Sys.setenv(ORACLE_HOME="/usr/lib/oracle/12.1/client64")
+Sys.setenv(NLS_LANG="KOREAN_KOREA.AL32UTF8")
+
+Sys.setenv(OCI_INC="/usr/include/oracle/12.1/client64")
