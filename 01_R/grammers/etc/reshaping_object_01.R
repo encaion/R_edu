7f00d235caa1e51bc71d456df24a3e68c48b5e15
Seung Wook Kim <CEO@Rloha.io>
2018-09-24 21:30:25 +0900

    Update reshaping_object_01.R

--- 01_R/grammers/etc/reshaping_object_01.R
+++ 01_R/grammers/etc/reshaping_object_01.R
@@ -1,3 +1,4 @@
+# 1
 library("reshape2")
 library("splitstackshape")
 df = data.frame(aa = rep(c("A", "B", "C"),
@@ -6,3 +7,14 @@ df = data.frame(aa = rep(c("A", "B", "C"),
 
 df = aggregate(data = df, bb ~ aa, FUN = "paste", collapse = "-")
 as.data.frame(cSplit(df, splitCols = "bb", sep = "-"))
+
+# 2
+library("tidyr")
+library("dplyr")
+df = data.frame(c1 = rep(c("A", "B", "C"), 
+                         times = c(3, 4, 2)), 
+                c2 = c(1, 2, 1, 1, 4, 4, 2, 4, 4))
+df %>% 
+  group_by(c1) %>%
+  mutate(group = 1:n()) %>%
+  spread(group, c2)
