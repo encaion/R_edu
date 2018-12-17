9b04b69f32d72cda27a5f390d4fa41495914412c
Seung Wook Kim <CEO@Rloha.io>
2018-12-17 23:55:37 +0900

    Create hc_02_scatter_plot.R

---
+++ 01_R/visualization/highcharter/hc_02_scatter_plot.R
@@ -0,0 +1,5 @@
+hc_add_series_scatter(hc = highchart(),
+                      x = mtcars$wt,
+                      y = mtcars$mpg,
+                      z = mtcars$drat, 
+                      color = mtcars$hp)
