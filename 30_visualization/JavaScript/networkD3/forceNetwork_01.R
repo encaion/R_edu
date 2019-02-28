8392677ead253f6970b61a3dfc141e12a58c648e
Seung Wook Kim <CEO@Rloha.io>
2019-02-28 21:20:14 +0900

    Update forceNetwork_01.R

--- 30_visualization/JavaScript/networkD3/forceNetwork_01.R
+++ 30_visualization/JavaScript/networkD3/forceNetwork_01.R
@@ -5,3 +5,12 @@ links = read.csv("forceNetwork_links.csv", stringsAsFactors = FALSE)
 forceNetwork(Links = links, Nodes = nodes, Source = "source",
              Target = "target", Value = "value", NodeID = "name",
              Group = "group", opacity = 0.8)
+
+# custom
+forceNetwork(Links = df_links_sub, Nodes = df_nodes_sub, 
+             Source = "from", Target = "to", 
+             Value = "count", NodeID = "name",
+             Group = "group", 
+             opacity = 0.8, Nodesize = "size", fontSize = 15,
+             colourScale = JS("d3.scaleOrdinal(['#69C242', '#64BBE3']);"),
+             radiusCalculation = JS("d.nodesize"))
