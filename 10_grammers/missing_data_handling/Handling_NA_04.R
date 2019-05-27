e9056e839bcf32e93465a0af8e0bd05b4593ecf7
Seung Wook Kim <CEO@Rloha.io>
2019-05-27 16:33:00 +0900

    Update Handling_NA_04.R

--- 10_grammers/missing_data_handling/Handling_NA_04.R
+++ 10_grammers/missing_data_handling/Handling_NA_04.R
@@ -1,4 +1,12 @@
+# 1
 df = data.frame(x = rep(1:3, 2),
                 y = c(1, NA, 2, NA, 3, NA))
 df[which(is.na(df$y)), "y"] = df[which(is.na(df$y)) - 1, "y"]
+df
+
+# 2
+df = data.frame(x = rep(1:3, 2),
+                y = c(1, NA, 2, NA, 3, NA))
+library("zoo")
+df[, "y"] = na.locf(df$y)
 df
