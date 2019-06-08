40f46e39099e8f22d47c3e542e8541853a4a9802
Seung Wook Kim <CEO@Rloha.io>
2019-06-08 16:55:37 +0900

    Update memory.R

--- 10_grammers/etc/memory.R
+++ 10_grammers/etc/memory.R
@@ -1,6 +1,8 @@
 memory.size()
 # memory.limit()
 
+cat("GB =", round(sum(gc()[,2])/1024, 3), "\n")
+# Ncells + Vcells
 
 # source: https://stackoverflow.com/questions/6457290/how-to-check-the-amount-of-ram-in-r
 # R> memfree <- as.numeric(system("awk '/MemFree/ {print $2}' /proc/meminfo", intern=TRUE))
