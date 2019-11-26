a3d9dff16fab3dbd3625e4c0c14096141dd7e4a7
Seung Wook Kim <CEO@Rloha.io>
2019-11-26 15:01:40 +0900

    Update ch_10_hts.R

--- 41_timeseries/package_forecast/ch_10_hts.R
+++ 41_timeseries/package_forecast/ch_10_hts.R
@@ -1,6 +1,21 @@
 library("fpp2") # for visnights dataset
 library("hts") # Hierarchical Time Series
 tourism.hts = hts(visnights, characters = c(3, 5)) 
+
+str(tourism.hts)
+tourism.hts$labels
+# $`Level 0`
+# [1] "Total"
+# 
+# $`Level 1`
+# [1] "NSW" "OTH" "QLD" "SAU" "VIC" "WAU"
+# 
+# $`Level 2`
+# [1] "NSWMetro" "NSWNthCo" "NSWNthIn" "NSWSthCo" "NSWSthIn" "OTHMetro"
+# [7] "OTHNoMet" "QLDCntrl" "QLDMetro" "QLDNthCo" "SAUCoast" "SAUInner"
+# [13] "SAUMetro" "VICEstCo" "VICInner" "VICMetro" "VICWstCo" "WAUCoast"
+# [19] "WAUInner" "WAUMetro"
+
 tourism.hts %>% 
   aggts(levels = 0:1) %>%
   autoplot(facet = TRUE) +
