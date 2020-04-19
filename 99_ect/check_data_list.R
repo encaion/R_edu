5f6aacdd9deaeddc4ea28cf41d4993d1b4402bd7
encaion <encaionkim@gmail.com>
2020-04-19 15:33:09 +0900

    update 200419

--- 99_ect/check_data_list.R
+++ 99_ect/check_data_list.R
@@ -1,2 +1,12 @@
 dd = data()$results[, 3]
 dd
+
+col_check = function(x){
+  x = gsub(pattern = " .*?$", replacement = "", x)
+  df = get(x)
+  info = list(dim = dim(df),
+              colnames = colnames(df))
+  return(info)
+}
+
+lapply(dd[4:8], FUN = "col_check")
