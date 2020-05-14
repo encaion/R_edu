d9b480e0d79eef0a25a53cae1c4bd9f3f26c46c7
Seung Wook Kim <CEO@Rloha.io>
2020-05-14 11:28:35 +0900

    Update AWS_RDS_mariaDB_UDFs.R

--- 00_environment_setting/AWS_RDS_mariaDB_UDFs.R
+++ 00_environment_setting/AWS_RDS_mariaDB_UDFs.R
@@ -14,3 +14,17 @@ table_length = function(table, conn = con){
   query = paste0("SELECT count(*) FROM ", table)
   return(dbGetQuery(conn = conn, query))
 }
+
+insert_query_gen = function(table, df){
+  classes = sapply(df, FUN = "is.numeric")
+  classes = as.numeric(which(classes == FALSE))
+  for(n_col in classes){
+    df_iris[, n_col] = as.character(df[, n_col])
+    df_iris[, n_col] = paste0("'", df[, n_col], "'")
+  }
+  colname = paste0("(timestamp, ", paste(colnames(df), collapse = ", "), ")")
+  value = apply(df, MARGIN = 1, FUN = function(x){paste0("(CURRENT_TIMESTAMP, ", paste(x, collapse = ", "), ")")})
+  value = paste(value, collapse = ", ")
+  query = paste("INSERT INTO", table, colname, "VALUES", value, sep = " ")
+  return(query)
+}
