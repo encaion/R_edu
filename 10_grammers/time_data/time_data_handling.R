199e848348d451b6116c759e00ed49285b911afa
Seung Wook Kim <CEO@Rloha.io>
2020-05-28 12:31:17 +0900

    Update time_data_handling.R

--- 10_grammers/time_data/time_data_handling.R
+++ 10_grammers/time_data/time_data_handling.R
@@ -11,3 +11,11 @@ unclass(aa)
 unclass(bb)
 
 as.POSIXct(1490799600, origin = "1970-01-01 00:00:00")
+
+library("lubridate")
+tt = ymd("2020-05-28")
+paste0(as.numeric(tt), "000")
+
+as_datetime(1587443975)
+as_datetime(0000000001)
+as.POSIXct(1, origin = "1970-01-01", tz = "UTC")
