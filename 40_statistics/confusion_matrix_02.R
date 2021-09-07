e06d7dfc669b2f42ae555a44eb22019da60f2744
Seung Wook Kim <encaionkim@gmail.com>
2021-09-08 00:35:27 +0900

    Delete confusion_matrix_02.R

--- 40_statistics/confusion_matrix_02.R
+++
@@ -1,7 +0,0 @@
-data = c(rep(0, 990), rep(1, 10))
-df = data.frame(cancer = data,
-                diagnosis = c(data[1:988], rep(1, 4), rep(0, 8)))
-ddd = table(df)
-
-library("caret")
-confusionMatrix(ddd)
